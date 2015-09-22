18033211c336cdf461d3d035d25f96e643d019fa
Tatu Lund <tatu@vaadin.com>
2015-09-22 12:31:59 +0300

    Added support for Converter

--- grid-renderers-collection-addon/src/main/java/org/vaadin/grid/cellrenderers/TextFieldRenderer.java
+++ grid-renderers-collection-addon/src/main/java/org/vaadin/grid/cellrenderers/TextFieldRenderer.java
@@ -5,14 +5,15 @@ import org.vaadin.grid.cellrenderers.client.TextFieldRendererState;
 
 import com.vaadin.data.Item;
 import com.vaadin.data.Property;
+import com.vaadin.data.util.converter.Converter;
 import com.vaadin.ui.renderers.ClickableRenderer;
 
 
-public class TextFieldRenderer extends ClickableRenderer<String>
+public class TextFieldRenderer<T> extends ClickableRenderer<T>
 {
     public TextFieldRenderer()
     {
-        super(String.class);
+        super((Class<T>) Object.class);
      
         registerRpc(new TextFieldRendererServerRpc()
         {
@@ -26,13 +27,20 @@ public class TextFieldRenderer extends ClickableRenderer<String>
                 Item row = getParentGrid().getContainerDataSource().getItem(itemId);
 
                 @SuppressWarnings("unchecked")
-                Property<String> cell = (Property<String>) row.getItemProperty(columnPropertyId);
+                Property<Object> cell = (Property<Object>) row.getItemProperty(columnPropertyId);
 
-                cell.setValue(newValue);
+                Class<T> targetType = (Class<T>) cell.getType();
+                Converter<String, T> converter = (Converter<String, T>) getColumn(columnId).getConverter();
+                if (converter != null) {
+                	cell.setValue(converter.convertToModel(newValue, targetType, getParentGrid().getLocale()));
+                } else if (targetType == String.class) {
+                	cell.setValue(newValue);
+                }
             }
 
         });
     }
+
 
     @Override
     protected TextFieldRendererState getState()
