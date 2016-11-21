8867985aef31b5cd458c8a49bd761645f184d9ac
Thorben von Hacht <thorben.vonhacht@bonprix.net>
2016-11-21 12:11:21 +0100

    jp-173: updated demo

--- grid-renderers-collection-addon/src/main/java/org/vaadin/grid/cellrenderers/client/editable/TextFieldRendererConnector.java
+++ grid-renderers-collection-addon/src/main/java/org/vaadin/grid/cellrenderers/client/editable/TextFieldRendererConnector.java
@@ -1,142 +1,154 @@
 package org.vaadin.grid.cellrenderers.client.editable;
 
-import com.google.gwt.dom.client.Style;
-import com.vaadin.client.VConsole;
-import com.vaadin.client.annotations.OnStateChange;
 import org.vaadin.grid.cellrenderers.editable.TextFieldRenderer;
 
-import com.google.gwt.core.client.GWT; 
-import com.google.gwt.dom.client.BrowserEvents; 
-import com.google.gwt.dom.client.Element; 
-import com.google.gwt.event.dom.client.*; 
-import com.google.web.bindery.event.shared.HandlerRegistration; 
-import com.vaadin.client.communication.RpcProxy; 
-import com.vaadin.client.connectors.ClickableRendererConnector; 
-import com.vaadin.client.connectors.GridConnector; 
-import com.vaadin.client.renderers.ClickableRenderer; 
-import com.vaadin.client.renderers.ClickableRenderer.RendererClickHandler; 
-import com.vaadin.client.renderers.Renderer; 
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.BrowserEvents;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.Style;
+import com.google.gwt.event.dom.client.ChangeEvent;
+import com.google.gwt.event.dom.client.ChangeHandler;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.MouseDownEvent;
+import com.google.gwt.event.dom.client.MouseDownHandler;
+import com.google.web.bindery.event.shared.HandlerRegistration;
+import com.vaadin.client.communication.RpcProxy;
+import com.vaadin.client.connectors.ClickableRendererConnector;
+import com.vaadin.client.connectors.GridConnector;
+import com.vaadin.client.renderers.ClickableRenderer;
+import com.vaadin.client.renderers.ClickableRenderer.RendererClickHandler;
+import com.vaadin.client.renderers.Renderer;
 import com.vaadin.client.ui.VTextField;
-import com.vaadin.client.widget.grid.RendererCellReference; 
-import com.vaadin.client.widgets.Grid; 
-import com.vaadin.shared.ui.Connect; 
+import com.vaadin.client.widget.grid.RendererCellReference;
+import com.vaadin.client.widgets.Grid;
+import com.vaadin.shared.ui.Connect;
 
 import elemental.json.JsonObject;
 
 @Connect(TextFieldRenderer.class)
-public class TextFieldRendererConnector extends ClickableRendererConnector<String>  {
-    TextFieldRendererServerRpc rpc = RpcProxy.create(
-            TextFieldRendererServerRpc.class, this);
+public class TextFieldRendererConnector extends ClickableRendererConnector<String> {
+	TextFieldRendererServerRpc rpc = RpcProxy.create(TextFieldRendererServerRpc.class, this);
 
-    public class TextFieldClientRenderer extends ClickableRenderer<String, VTextField> {
+	public class TextFieldClientRenderer extends ClickableRenderer<String, VTextField> {
 
-        private static final String ROW_KEY_PROPERTY = "rowKey";
-        private static final String COLUMN_ID_PROPERTY = "columnId";
+		private static final String ROW_KEY_PROPERTY = "rowKey";
+		private static final String COLUMN_ID_PROPERTY = "columnId";
 
-        private boolean doesTextFieldContainValue(VTextField textField, String value) {
-            if(textField.getValue().equals(value)) {
-            	return true;
-            }
-            return false;
-        }
+		private boolean doesTextFieldContainValue(VTextField textField, String value) {
+			if (textField	.getValue()
+							.equals(value)) {
+				return true;
+			}
+			return false;
+		}
 
-        @Override
-        public void render(RendererCellReference cell, String selectedValue,
-                           VTextField textField) {
+		@Override
+		public void render(RendererCellReference cell, String selectedValue, VTextField textField) {
 
-            Element e = textField.getElement();
+			Element e = textField.getElement();
 
-            getState().value = selectedValue;
-            
-            if(e.getPropertyString(ROW_KEY_PROPERTY) != getRowKey((JsonObject) cell.getRow())) {
-                e.setPropertyString(ROW_KEY_PROPERTY,
-                        getRowKey((JsonObject) cell.getRow()));
-            }
-            // Generics issue, need a correctly typed column.
+			getState().value = selectedValue;
 
-            if(e.getPropertyString(COLUMN_ID_PROPERTY) != getColumnId(getGrid()
-                    .getColumn(cell.getColumnIndex()))) {
-                e.setPropertyString(COLUMN_ID_PROPERTY, getColumnId(getGrid()
-                        .getColumn(cell.getColumnIndex())));
-            }
+			if (e.getPropertyString(ROW_KEY_PROPERTY) != getRowKey((JsonObject) cell.getRow())) {
+				e.setPropertyString(ROW_KEY_PROPERTY, getRowKey((JsonObject) cell.getRow()));
+			}
+			// Generics issue, need a correctly typed column.
 
-            textField.setValue(selectedValue);
-            
-            if(textField.isEnabled() != cell.getColumn().isEditable()) {
-                textField.setEnabled(cell.getColumn().isEditable());
-            }
-        }
+			if (e.getPropertyString(COLUMN_ID_PROPERTY) != getColumnId(getGrid().getColumn(cell.getColumnIndex()))) {
+				e.setPropertyString(COLUMN_ID_PROPERTY, getColumnId(getGrid().getColumn(cell.getColumnIndex())));
+			}
 
-        @Override
-        public VTextField createWidget() {
-            VTextField textField = GWT.create(VTextField.class);
+			textField.setValue(selectedValue);
 
-            if(getState().fitToCell) {
-                textField.setWidth("100%");
+			if (textField.isEnabled() != cell	.getColumn()
+												.isEditable()) {
+				textField.setEnabled(cell	.getColumn()
+											.isEditable());
+			}
+		}
 
-                textField.getElement().getStyle().setPosition(Style.Position.RELATIVE);
-                textField.getElement().getStyle().setProperty("border-radius", "0");
-                textField.getElement().getStyle().setProperty("padding", "0 16px");
-                textField.getElement().getStyle().setLeft(-16.0, Style.Unit.PX);
-                textField.getElement().getStyle().setProperty("border", "none");
-                textField.getElement().getStyle().setTop(-1, Style.Unit.PX);
-            }
+		@Override
+		public VTextField createWidget() {
+			VTextField textField = GWT.create(VTextField.class);
 
-            textField.sinkBitlessEvent(BrowserEvents.CLICK);
-            textField.sinkBitlessEvent(BrowserEvents.MOUSEDOWN);
+			if (getState().fitToCell) {
+				textField.setWidth("100%");
 
-            textField.addChangeHandler(new ChangeHandler() {
-                @Override
-                public void onChange(ChangeEvent changeEvent) {
-                    VTextField textField = (VTextField) changeEvent.getSource();
-                    Element e = textField.getElement();
-                    rpc.onChange(e.getPropertyString(ROW_KEY_PROPERTY),
-                            e.getPropertyString(COLUMN_ID_PROPERTY),
-                            textField.getValue());
-                }
-            });
+				textField	.getElement()
+							.getStyle()
+							.setPosition(Style.Position.RELATIVE);
+				textField	.getElement()
+							.getStyle()
+							.setProperty("border-radius", "0");
+				textField	.getElement()
+							.getStyle()
+							.setProperty("padding", "0 16px");
+				textField	.getElement()
+							.getStyle()
+							.setLeft(-16.0, Style.Unit.PX);
+				textField	.getElement()
+							.getStyle()
+							.setProperty("border", "none");
+				textField	.getElement()
+							.getStyle()
+							.setTop(-1, Style.Unit.PX);
+			}
 
-            textField.addClickHandler(new ClickHandler() {
-                @Override
-                public void onClick(ClickEvent event) {
-                    event.stopPropagation();
-                }
-            });
+			textField.sinkBitlessEvent(BrowserEvents.CLICK);
+			textField.sinkBitlessEvent(BrowserEvents.MOUSEDOWN);
 
-            textField.addMouseDownHandler(new MouseDownHandler() {
-                @Override
-                public void onMouseDown(MouseDownEvent event) {
-                    event.stopPropagation();
-                }
-            });
+			textField.addChangeHandler(new ChangeHandler() {
+				@Override
+				public void onChange(ChangeEvent changeEvent) {
+					VTextField textField = (VTextField) changeEvent.getSource();
+					Element e = textField.getElement();
+					TextFieldRendererConnector.this.rpc.onChange(	e.getPropertyString(ROW_KEY_PROPERTY),
+																	e.getPropertyString(COLUMN_ID_PROPERTY),
+																	textField.getValue());
+				}
+			});
 
-            return textField;
-        }
-    }
-    
-    @Override
-    public TextFieldRendererState getState() {
-    	return (TextFieldRendererState) super.getState();
-    }
-    
-    @Override
-    protected Renderer<String> createRenderer() {
-        return new TextFieldClientRenderer();
-    }
+			textField.addClickHandler(new ClickHandler() {
+				@Override
+				public void onClick(ClickEvent event) {
+					event.stopPropagation();
+				}
+			});
 
-    @Override
-    public TextFieldClientRenderer getRenderer() {
-        return (TextFieldClientRenderer) super.getRenderer();
-    }
+			textField.addMouseDownHandler(new MouseDownHandler() {
+				@Override
+				public void onMouseDown(MouseDownEvent event) {
+					event.stopPropagation();
+				}
+			});
 
-    @Override
-    protected HandlerRegistration addClickHandler(
-            RendererClickHandler<JsonObject> handler) {
-        return getRenderer().addClickHandler(handler);
-    }
+			return textField;
+		}
+	}
 
-    private Grid<JsonObject> getGrid() {
-        return ((GridConnector) getParent()).getWidget();
-    }
+	@Override
+	public TextFieldRendererState getState() {
+		return (TextFieldRendererState) super.getState();
+	}
+
+	@Override
+	protected Renderer<String> createRenderer() {
+		return new TextFieldClientRenderer();
+	}
+
+	@Override
+	public TextFieldClientRenderer getRenderer() {
+		return (TextFieldClientRenderer) super.getRenderer();
+	}
+
+	@Override
+	protected HandlerRegistration addClickHandler(RendererClickHandler<JsonObject> handler) {
+		return getRenderer().addClickHandler(handler);
+	}
+
+	private Grid<JsonObject> getGrid() {
+		return ((GridConnector) getParent()).getWidget();
+	}
 
 }
