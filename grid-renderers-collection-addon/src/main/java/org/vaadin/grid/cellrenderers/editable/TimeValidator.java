4f435d5d1c77780c9b8786b783fdf8654c6899f0
Torben Meissner <torben.meissner@bonprix.net>
2018-07-18 11:02:22 +0200

    create static Pattern

--- grid-renderers-collection-addon/src/main/java/org/vaadin/grid/cellrenderers/editable/TimeValidator.java
+++ grid-renderers-collection-addon/src/main/java/org/vaadin/grid/cellrenderers/editable/TimeValidator.java
@@ -1,10 +1,13 @@
 package org.vaadin.grid.cellrenderers.editable;
 
 import java.util.Locale;
+import java.util.regex.Pattern;
 
 import com.vaadin.data.util.converter.Converter;
 
 public class TimeValidator implements Converter<String, String> {
+
+    static Pattern TIME_PATTERN = Pattern.compile("^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$");
 
     @Override
     public String convertToModel(final String value, final Class<? extends String> targetType, final Locale locale) throws ConversionException {
@@ -28,7 +31,7 @@ public class TimeValidator implements Converter<String, String> {
     }
 
     private String returnValueIfMatchTime(final String value) {
-        if (value.matches("^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$")) {
+        if (value.trim().matches(TimeValidator.TIME_PATTERN.toString())) {
             return value;
         }
         throw new ConversionException();
